253b5e9b9d794e049a696bdb934a1e72f92372ac
Micah R Ledbetter <me@micahrl.com>
2025-10-29 16:23:01 -0500

    Add curltest.sh

---
+++ aws/curltest.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if test -z "$1" || test "$1" = "suns"; then
+    endpoint="https://suns.bz/v1/attest"
+else
+    endpoint="https://pz39hzelmj.execute-api.us-east-2.amazonaws.com/v1/attest"
+fi
+
+set -x
+
+curl -X POST "$endpoint" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "owner": "https://me.micahrl.com",
+    "type": "mirrornames",
+    "domains": ["me.micahrl.com", "com.micahrl.me"]
+  }'
