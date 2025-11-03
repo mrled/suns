5a803d1713824ca49a7ff042d5ffe710bd50748c
Micah R Ledbetter <me@micahrl.com>
2025-11-03 06:12:03 -0600

    Add attestation record

--- aws/lib/DnsStack.ts
+++ aws/lib/DnsStack.ts
@@ -48,5 +48,16 @@ export class DnsStack extends cdk.Stack {
         new targets.CloudFrontTarget(props.distribution),
       ),
     });
+
+    // Create TXT record for _suns.zq.suns.bz
+    new route53.TxtRecord(this, "SunsZqTxtRecord", {
+      zone: props.hostedZone,
+      recordName: "_suns.zq",
+      values: [
+        // flip180
+        "v1:b:Adg2VY1R7OIZHMz/Z1uk1lOCe2DtwzXYTpaBHpDEtkU=:AjlAANu+Rl8TYqK0CvGy+S6NIswJmRuEsYSn43MG+2s=",
+      ],
+      ttl: cdk.Duration.minutes(1),
+    });
   }
 }
